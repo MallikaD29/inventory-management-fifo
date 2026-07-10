44c719a4d448a9b8417a35c9ea312007cdd6e410
MallikaD29 <mallikadutta041@gmail.com>
2026-07-10 17:23:34 +0530

    Update production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://inventory-management-fifo.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
